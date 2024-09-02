a82971efcbc53383e0e184258c209d49d3190b6a
PimenovSerg <108987794+PimenovSerg@users.noreply.github.com>
2024-09-02 11:21:08 +0300

    fix commit

--- src/test/java/dev/pimenov/ci/MathTest.java
+++ src/test/java/dev/pimenov/ci/MathTest.java
@@ -28,9 +28,10 @@ class MathTest {
     @Test
     void  minus(){
         int ex = 10;
-        int ac = math.minusTwoDigits(12,2);
+        int ac = math.minusTwoDigits(12,3);
         assertEquals(ex,ac);
     }
+
 
 
 }
